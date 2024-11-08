6a2302ff819c273c3a7983d3f67fdc4047b470fc
Samuel Wu <twopizza9621536@gmail.com>
2024-11-08 16:32:15 -0500

    Add lab06 solutions
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

---
+++ src/labs/lab06/StringLinkedQueue.java
@@ -0,0 +1,63 @@
+/*
+ * Samuel Wu
+ * 2024-11-04
+ */
+
+package labs.lab06;
+
+public class StringLinkedQueue {
+    private class StringNode {
+        String data;
+        StringNode link;
+
+        public StringNode(String data, StringNode link) {
+            this.data = data;
+            this.link = link;
+        }
+    }
+
+    private StringNode head;
+    private StringNode tail;
+
+    public StringLinkedQueue() {
+        head = null;
+        tail = null;
+    }
+
+    public void enqueue(String data) {
+        if (data == null) {
+            return;
+        }
+
+        StringNode temp = new StringNode(data, null);
+
+        if (head == null) {
+            head = temp;
+            tail = head;
+            return;
+        }
+
+        tail.link = temp;
+        tail = tail.link;
+    }
+
+    public String dequeue() {
+        if (head == null) {
+            return null;
+        }
+
+        String data = head.data;
+        head = head.link;
+        return data;
+    }
+
+    public int countStrings() {
+        int count = 0;
+
+        for (StringNode temp = head; temp != null; temp = temp.link) {
+            count++;
+        }
+
+        return count;
+    }
+}
