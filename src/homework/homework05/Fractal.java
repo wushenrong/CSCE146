24df699e94d2fef650da206407210955ad20d10f
Samuel Wu <twopizza9621536@gmail.com>
2024-11-15 18:09:30 -0500

    Finalize solution for homework05
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- src/homework/homework05/Fractal.java
+++ src/homework/homework05/Fractal.java
@@ -8,11 +8,18 @@ package homework.homework05;
 import javax.swing.JFrame;
 
 public class Fractal {
+    public static final int SCREEN_WIDTH = 400;
+    public static final int SCREEN_HEIGHT = 400;
+
+    /**
+     * Main method to create a new frame and canvas to show and draw the
+     * sierpinski triangle on respectively.
+     */
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Fractal");
         SierpinskiTriangle triangle = new SierpinskiTriangle();
 
-        mainFrame.setSize(900, 900);
+        mainFrame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         mainFrame.setVisible(true);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.add(triangle);
